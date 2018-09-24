60086f3acc6083fe01cc7e89c05feaf708a79e87
Bryce A Menadue <bryce.menadue98@gmail.com>
2018-09-24 14:08:56 -0500

    week 4 work
    added validation and styling

---
+++ PHP/masterStyle/labs/week04/sendemail.php
@@ -0,0 +1,78 @@
+<!DOCTYPE>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Make Me Elvis - Send Email</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div class="parallax">
+<img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+<img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+<p><strong>Private:</strong> For Elmer's use ONLY<br />
+    Write and send an email to mailing list members.</p>
+
+<?php
+    if (isset($_POST['submit'])) {
+        $from = 'bryce.menadue98@gmail.com';
+        $subject = $_POST['subject'];
+        $text = $_POST['elvismail'];
+        $output_form = false;
+
+        if (empty($subject) && empty($text)) {
+            // We know both $subject AND $text are blank
+            echo 'You forgot the email subject and body text.<br />';
+            $output_form = true;
+        }
+
+        if (empty($subject) && (!empty($text))) {
+            echo 'You forgot the email subject.<br />';
+            $output_form = true;
+        }
+
+        if ((!empty($subject)) && empty($text)) {
+            echo 'You forgot the email body text.<br />';
+            $output_form = true;
+        }
+
+        if ((!empty($subject)) && (!empty($text))) {
+            $dbc = mysqli_connect('softdev.mstclab.com', 'bmenadue', 'bmenadue', 'bmenadue')
+            or die('Error connecting to MySQL server.');
+
+            $query = "SELECT * FROM email_list";
+            $result = mysqli_query($dbc, $query)
+            or die('Error querying database.');
+
+            while ($row = mysqli_fetch_array($result)){
+                $to = $row['email'];
+                $first_name = $row['first_name'];
+                $last_name = $row['last_name'];
+                $msg = "Dear $first_name $last_name,\n$text";
+                mail($to, $subject, $msg, 'From:' . $from);
+                echo 'Email sent to: ' . $to . '<br />';
+            }
+            mysqli_close($dbc);
+        }
+    }
+    else {
+         $output_form = true;
+    }
+
+if ($output_form) {
+    ?>
+
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="subject">Subject of email:</label><br/>
+        <input id="subject" name="subject" type="text" size="30" value="<?php echo $subject; ?>"/><br/>
+        <label for="elvismail">Body of email:</label><br/>
+        <textarea id="elvismail" name="elvismail" rows="8" cols="40"> <?php echo $text; ?></textarea><br/>
+        <input type="submit" name="submit" value="Submit"/>
+    </form>
+
+    <?php
+}
+?>
+</div>
+</body>
+</html>
+
